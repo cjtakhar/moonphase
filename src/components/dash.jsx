6eb9566fdc1471e340649f127edb68d425b19274
cjtakhar <cjtakhar@gmail.com>
2023-05-19 20:57:38 -0700

    css

--- src/components/dash.jsx
+++ src/components/dash.jsx
@@ -6,7 +6,7 @@ const Dash = () => {
         <div className="dash-container">
             <div className="box1">
                 <h1 className="box1-title">Welcome!</h1>
-                <p className="box1-paragraph">This is the psychic sqrl network. A hot new guide to the mystic arts. Explore the celestial light of tarot, spells, protection, and healing through our astrological insights. Enter your birthday and find yourself.</p>
+                <p className="box1-paragraph">This is Moon Phase. A hot new guide to the mystic arts. Explore the celestial light of tarot, spells, protection, and healing. Immerse yourself in the spiritual insights of the ethereal moon dance with our lunar phase journal. Enter your birthday and discover the profound connection waiting for you.</p>
                 <p className="box1-paragraph2">What are you waiting for?</p>
                 <div className="btn-container">
                 <Link to="/login">
